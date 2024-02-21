25c6f06b528e8cca089196a0dd34d966c352a4f8
jeffquinn-msk <105746884+jeffquinn-msk@users.noreply.github.com>
2024-02-21 16:32:33 -0500

    Add Quiver Plot (#22)

--- src/nuc2seg/cli/plot_predictions.py
+++ src/nuc2seg/cli/plot_predictions.py
@@ -2,6 +2,7 @@ import argparse
 import logging
 import os.path
 import tqdm
+import numpy as np
 
 from nuc2seg import log_config
 from nuc2seg.data import Nuc2SegDataset, TiledDataset, ModelPredictions, generate_tiles
@@ -51,6 +52,12 @@ def get_parser():
         type=str,
         required=True,
     )
+    parser.add_argument(
+        "--max-plots",
+        help="Number of plots to make",
+        type=int,
+        default=100,
+    )
     return parser
 
 
@@ -90,6 +97,9 @@ def main():
     os.makedirs(args.output_dir, exist_ok=True)
 
     for bbox in tqdm.tqdm(tile_generator, total=len(tiled_dataset), unit="plot"):
+        if np.count_nonzero(ds.labels[bbox[0] : bbox[2], bbox[1] : bbox[3]]) == 0:
+            continue
+
         plot_model_predictions(
             dataset=ds,
             model_predictions=predictions,
@@ -97,4 +107,5 @@ def main():
             output_path=os.path.join(
                 args.output_dir, "_".join([str(x) for x in bbox]) + ".pdf"
             ),
+            use_quiver=True,
         )
