da60f51fd8b064c595c56363da0d3631c58407dc
jeffquinn-msk <quinnj2@mskcc.org>
2024-03-05 17:11:57 -0500

    write and test stitching scheme

---
+++ src/nuc2seg/cli/baysor_postprocess.py
@@ -0,0 +1,97 @@
+import argparse
+import logging
+import geopandas as gpd
+import pandas
+import math
+
+from nuc2seg import log_config
+from nuc2seg.postprocess import stitch_shapes
+
+logger = logging.getLogger(__name__)
+
+
+def get_parser():
+    parser = argparse.ArgumentParser(
+        description="Benchmark cell segmentation given post-Xenium IF data that includes an autofluorescence marker."
+    )
+    log_config.add_logging_args(parser)
+    parser.add_argument(
+        "--baysor-shapefiles",
+        help="One or more shapefiles output by baysor.",
+        type=str,
+        required=True,
+        nargs="+",
+    )
+
+    parser.add_argument(
+        "--transcripts",
+        help="Xenium transcripts in parquet format.",
+        type=str,
+        required=True,
+    )
+    parser.add_argument(
+        "--output",
+        required=True,
+        type=str,
+        help="Output file.",
+    )
+    parser.add_argument(
+        "--sample-area",
+        default=None,
+        type=str,
+        help='Crop the dataset to this rectangle, provided in in "x1,y1,x2,y2" format.',
+    )
+    parser.add_argument(
+        "--tile-height",
+        help="Height of the tiles.",
+        type=int,
+        default=64,
+    )
+    parser.add_argument(
+        "--tile-width",
+        help="Width of the tiles.",
+        type=int,
+        default=64,
+    )
+    parser.add_argument(
+        "--overlap-percentage",
+        help="What percent of each tile dimension overlaps with the next tile.",
+        type=float,
+        default=0.25,
+    )
+
+    return parser
+
+
+def get_args():
+    parser = get_parser()
+
+    args = parser.parse_args()
+
+    return args
+
+
+def main():
+    args = get_args()
+
+    log_config.configure_logging(args)
+
+    transcript_df = pandas.read_parquet(args.transcripts)
+
+    x_extent = math.ceil(transcript_df["x_location"].astype(float).max())
+    y_extent = math.ceil(transcript_df["y_location"].astype(float).max())
+
+    shapefiles = sorted(
+        args.baysor_shapefiles, key=lambda x: int(x.split("_")[-1].split(".")[0])
+    )
+
+    gdfs = [gpd.read_file(shapefile) for shapefile in shapefiles]
+
+    stitched_shapes = stitch_shapes(
+        shapes=gdfs,
+        tile_size=(args.tile_width, args.tile_height),
+        base_size=(x_extent, y_extent),
+        overlap=args.overlap_percentage,
+    )
+
+    stitched_shapes.to_parquet(args.output)
