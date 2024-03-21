a7d7e0ee56712cdc595b1705fe024fbe0956ad04
Quinn <quinnj2@lbst0162.mskcc.root.mskcc.org>
2024-03-21 14:37:26 -0400

    done

--- src/nuc2seg/cli/baysor_postprocess.py
+++ src/nuc2seg/cli/baysor_postprocess.py
@@ -3,9 +3,13 @@ import logging
 import geopandas as gpd
 import pandas
 import math
+import os.path
+
 
 from nuc2seg import log_config
-from nuc2seg.postprocess import stitch_shapes
+from nuc2seg.postprocess import stitch_shapes, read_baysor_results
+from nuc2seg.plotting import plot_final_segmentation, plot_segmentation_class_assignment
+from nuc2seg.xenium import load_nuclei
 
 logger = logging.getLogger(__name__)
 
@@ -22,7 +26,19 @@ def get_parser():
         required=True,
         nargs="+",
     )
-
+    parser.add_argument(
+        "--baysor-transcript-assignments",
+        help="One or more transcript assignment files output by baysor.",
+        type=str,
+        required=True,
+        nargs="+",
+    )
+    parser.add_argument(
+        "--nuclei-file",
+        help="Path to the Xenium nuclei boundaries parquet file",
+        type=str,
+        required=True,
+    )
     parser.add_argument(
         "--transcripts",
         help="Xenium transcripts in parquet format.",
@@ -33,7 +49,7 @@ def get_parser():
         "--output",
         required=True,
         type=str,
-        help="Output file.",
+        help="Output path.",
     )
     parser.add_argument(
         "--sample-area",
@@ -85,13 +101,41 @@ def main():
         args.baysor_shapefiles, key=lambda x: int(x.split("_")[-1].split(".")[0])
     )
 
-    gdfs = [gpd.read_file(shapefile) for shapefile in shapefiles]
+    transcript_assignment_files = sorted(
+        args.baysor_transcript_assignments,
+        key=lambda x: int(x.split("_")[-1].split(".")[0]),
+    )
+
+    if len(shapefiles) != len(transcript_assignment_files):
+        raise ValueError(
+            "Number of shapefiles and transcript assignment files must be the same."
+        )
+
+    zipped_fns = zip(shapefiles, transcript_assignment_files)
+
+    gdfs = [read_baysor_results(*args) for args in zipped_fns]
 
     stitched_shapes = stitch_shapes(
         shapes=gdfs,
         tile_size=(args.tile_width, args.tile_height),
         base_size=(x_extent, y_extent),
         overlap=args.overlap_percentage,
+    )
+
+    nuclei_geo_df = load_nuclei(
+        nuclei_file=args.nuclei_file,
+        sample_area=None,
+    )
+
+    plot_final_segmentation(
+        nuclei_gdf=nuclei_geo_df,
+        segmentation_gdf=stitched_shapes,
+        output_path=os.path.join(os.path.dirname(args.output), "segmentation.png"),
+    )
+    plot_segmentation_class_assignment(
+        segmentation_gdf=stitched_shapes,
+        output_path=os.path.join(os.path.dirname(args.output), "class_assignment.png"),
+        cat_column="cluster",
     )
 
     stitched_shapes.to_parquet(args.output)
