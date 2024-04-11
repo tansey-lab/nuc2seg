b992964eac396063cd5ccf8d3afd52f3b6fd4488
jeffquinn-msk <105746884+jeffquinn-msk@users.noreply.github.com>
2024-04-11 14:25:38 -0400

    Set a Reasonable Default for N Dataloaders (#98)
    Co-authored-by: Quinn <quinnj2@lbst0162.mskcc.root.mskcc.org>

--- src/nuc2seg/cli/predict.py
+++ src/nuc2seg/cli/predict.py
@@ -59,7 +59,7 @@ def get_parser():
         "--num-dataloader-workers",
         help="Number of workers to use for the data loader.",
         type=int,
-        default=0,
+        default=2,
     )
     parser.add_argument(
         "--device",
