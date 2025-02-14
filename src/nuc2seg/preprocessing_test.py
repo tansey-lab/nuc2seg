75746d0662d6e8d7ebdf1afde9e737a7871901d9
Jeff Quinn <quinnj2@mskcc.org>
2025-02-14 16:30:09 -0500

    new preprocessing works

--- src/nuc2seg/preprocessing_test.py
+++ src/nuc2seg/preprocessing_test.py
@@ -9,7 +9,11 @@ import pandas
 import pytest
 import shapely
 
-from nuc2seg.preprocessing import create_rasterized_dataset, tile_transcripts_to_disk
+from nuc2seg.preprocessing import (
+    create_rasterized_dataset,
+    tile_transcripts_to_disk,
+    create_pixel_geodf,
+)
 
 
 @pytest.fixture(scope="function")
@@ -161,25 +165,26 @@ def test_transcripts_df():
 def test_create_rasterized_dataset(test_nuclei_df, test_transcripts_df):
     np.random.seed(0)
     ds = create_rasterized_dataset(
-        nuclei_geo_df=test_nuclei_df,
+        prior_segmentation_gdf=test_nuclei_df,
         tx_geo_df=test_transcripts_df,
         sample_area=shapely.Polygon([(1, 1), (30, 1), (30, 20), (1, 20), (1, 1)]),
         resolution=1,
-        foreground_nucleus_distance=1,
-        background_nucleus_distance=5,
+        foreground_distance=1,
+        background_distance=4,
         background_transcript_distance=3,
         background_pixel_transcripts=5,
     )
 
-    assert ds.labels.shape == (30, 20)
+    assert ds.labels.shape == (29, 19)
     assert ds.transcripts.shape == (8, 3)
-    assert ds.x_extent_pixels == 30
-    assert ds.y_extent_pixels == 20
+    assert ds.x_extent_pixels == 29
+    assert ds.y_extent_pixels == 19
     assert ds.n_genes == 2
 
     # Assert coordinated are transformed relative to the bbox
     assert ds.transcripts[:, 0].min() == 9.0
     assert ds.transcripts[:, 1].min() == 4.0
+    assert sorted(np.unique(ds.labels).tolist()) == [-1, 0, 1, 2]
 
 
 def test_tile_transcripts_to_csv(test_transcripts_df):
@@ -201,3 +206,13 @@ def test_tile_transcripts_to_csv(test_transcripts_df):
             df = pandas.read_csv(fn)
     finally:
         shutil.rmtree(output_dir)
+
+
+def test_create_pixel_geodf():
+    result = create_pixel_geodf(
+        width=12,
+        height=12,
+        resolution=5,
+    )
+    assert tuple(result.total_bounds) == (0, 0, 15, 15)
+    assert len(result) == 9
