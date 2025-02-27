Fix Radians Units (#212)

src/nuc2seg/segment.py
@@ -22,7 +22,11 @@ from nuc2seg.data import (
     SegmentationResults,
 )
 from nuc2seg.preprocessing import pol2cart
-from nuc2seg.utils import get_indices_for_ndarray, create_torch_polygon
+from nuc2seg.utils import (
+    get_indices_for_ndarray,
+    create_torch_polygon,
+    normalized_radians_to_radians,
+)
 
 logger = logging.getLogger(__name__)
 
@@ -363,8 +367,12 @@ def greedy_cell_segmentation(
     x_indices = indices_2d[:, 0]
     y_indices = indices_2d[:, 1]
 
-    flow_xy = flow_destination(indices_2d, predictions.angles, flow1_magnitude)
-    flow_xy2 = flow_destination(indices_2d, predictions.angles, flow2_magnitude)
+    flow_xy = flow_destination(
+        indices_2d, normalized_radians_to_radians(predictions.angles), flow1_magnitude
+    )
+    flow_xy2 = flow_destination(
+        indices_2d, normalized_radians_to_radians(predictions.angles), flow2_magnitude
+    )
 
     # Get the pixels that are sufficiently predicted to be foreground
     foreground_mask = (predictions.foreground >= foreground_threshold)[
@@ -783,7 +791,9 @@ def ray_tracing_cell_segmentation(
     use_labels=True,
     use_early_stopping=True,
 ):
-    angles = torch.tensor(predictions.angles, device=device)
+    angles = torch.tensor(
+        normalized_radians_to_radians(predictions.angles), device=device
+    )
     polygons: list[shapely.Polygon] = segmentation_array_to_shapefile(
         dataset.labels
     ).geometry.to_list()

src/nuc2seg/segment_test.py
@@ -165,7 +165,7 @@ def test_greedy_cell_segmentation_early_stop():
             x_component = 32 - x
             y_component = 32 - y
             angle = cart2pol(x=x_component, y=y_component)
-            angles[x, y] = angle[1]
+            angles[x, y] = (angle[1] + np.pi) / (2 * np.pi)
 
     ds = Nuc2SegDataset(
         labels=labels.astype(int),
@@ -211,7 +211,7 @@ def test_ray_tracing_segmentation_early_stop():
             x_component = 32 - x
             y_component = 25 - y
             angle = cart2pol(x=x_component, y=y_component)
-            angles[x, y] = angle[1]
+            angles[x, y] = (angle[1] + np.pi) / (2 * np.pi)
 
     ds = Nuc2SegDataset(
         labels=labels.astype(int),
@@ -488,7 +488,7 @@ def test_label_free_greedy_expansion():
             x_component = (x_extent_pixels / 2) - (x + 0.5)
             y_component = (y_extent_pixels / 2) - (y + 0.5)
             angle = cart2pol(x=x_component, y=y_component)
-            angles[x, y] = angle[1]
+            angles[x, y] = (angle[1] + np.pi) / (2 * np.pi)
 
     start_xy = np.mgrid[0:x_extent_pixels, 0:y_extent_pixels]
     start_xy = np.array(list(zip(start_xy[0].flatten(), start_xy[1].flatten())))